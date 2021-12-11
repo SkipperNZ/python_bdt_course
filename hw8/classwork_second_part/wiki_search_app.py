6a14e589581ea236f93176c5f1f1ec9bee7e0894
Nikita <38168210+SkipperNZ@users.noreply.github.com>
2021-12-11 19:41:51 +0300

    conspect for 8 week is done.

--- hw8/classwork_second_part/wiki_search_app.py
+++ hw8/classwork_second_part/wiki_search_app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request
+from flask import Flask, request, abort, jsonify , render_template
 import requests
+from lxml import etree
 
 app = Flask(__name__)
 
@@ -12,3 +13,48 @@ def wiki_proxy_search():
     user_query = request.args.get("query", "")
     wiki_response = requests.get(WIKI_BASE_SEARCH_URL + user_query )
     return wiki_response.text, wiki_response.status_code   
+
+
+@app.route("/pretty_search")
+def pretty_wiki_proxy_search():
+    user_query = request.args.get("query", "")
+    wiki_response = requests.get(WIKI_BASE_SEARCH_URL + user_query )
+    if not wiki_response.ok:
+        abort(503)
+
+    document_collection = parse_wiki_search_output(wiki_response.text)
+    return render_template(
+        "wiki_search_result.html",
+        query=user_query,
+        document_collection=document_collection,
+        wikipedia_base_url=WIKI_BASE_URL
+        )
+
+
+
+@app.route("/api/search")
+def api_wiki_proxy_search():
+    user_query = request.args.get("query", "")
+    wiki_response = requests.get(WIKI_BASE_SEARCH_URL + user_query )
+    if not wiki_response.ok:
+        abort(503)
+
+    documents = parse_wiki_search_output(wiki_response.text)
+    return jsonify({
+        "documents": documents,
+        "version": 1.0,
+    })  
+
+
+def parse_wiki_search_output(wiki_search_output):
+    root = etree.fromstring(wiki_search_output, etree.HTMLParser())
+    document_raw_collection = root.xpath("//li[@class='mw-search-result']")
+    document_collection = []
+    for document in document_raw_collection:
+        link = document.xpath(".//a[1]/@href")[0]
+        title = document.xpath(".//a[1]/@title")[0]
+        snippet = "".join(document.xpath(".//div[@class='searchresult']")[0].itertext())
+        document_collection.append([title, link, snippet])
+
+
+    return document_collection
